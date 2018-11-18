31c159b8e98578e9d381864b662326d8c9ca5fe2
chokyuhyun <jo@chokyuhyunui-MacBookAir.local>
2018-11-18 15:39:08 +0900

    update

--- index.js
+++ index.js
@@ -1,119 +1,97 @@
-"use strict"
+'use strict';
 
-let cho = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ".split('')
-let jung = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ".split('')
-let jong = "ㄱ/ㄲ/ㄱㅅ/ㄴ/ㄴㅈ/ㄴㅎ/ㄷ/ㄹ/ㄹㄱ/ㄹㅁ/ㄹㅂ/ㄹㅅ/ㄹㅌ/ㄹㅍ/ㄹㅎ/ㅁ/ㅂ/ㅂㅅ/ㅅ/ㅆ/ㅇ/ㅈ/ㅊ/ㅋ/ㅌ/ㅍ/ㅎ".split('/')
+const _ = require('lodash');
 
-let _ = require('lodash')
+module.exports = { encode, decode };
 
-function decompose(x) {
-    if (x < 0xac00 || x > 0xd7af) return String.fromCharCode(x)
-    x = x - 0xac00
-    let y = x / 28 | 0
-    let z = x % 28
-    x = y / 21 | 0
-    y = y % 21
-    let zz = z ? jong[z-1].split('') : null
-    return _.compact(_.flatten([cho[x],jung[y],zz]))
+const FIRST_KORAEN_CHAR = '\uac00';
+const LAST_KOREAN_CHAR = '\ud7af';
+const KOREAN_UNICODE_DEFAULT_INDEX = parseInt('0xac00', 16);
+///////////////////////////////////////
+// 유니코드 한글은 0xAC00 으로부터 
+// 초성 19개, 중상21개, 종성28개로 이루어지고
+// 이들을 조합한 11,172개의 문자를 갖음
+let h2Ascii = {};
+let ascii2H = {};
+{
+    const mapping = (s, d) => { h2Ascii[s] = d; ascii2H[d] = s; }
+    let DEFAULT_ASCII_INDEX = 48;
+    for (let i = 0, l = 19; i < l; i++) mapping((parseInt('0x1100', 16) + i), (DEFAULT_ASCII_INDEX++))
+    for (let i = 0, l = 21; i < l; i++) mapping((parseInt('0x1161', 16) + i), (DEFAULT_ASCII_INDEX++))
+    for (let i = 0, l = 28; i < l; i++) mapping((parseInt('0x11A8', 16) + i), (DEFAULT_ASCII_INDEX++))
 }
-function test(str) {
-    return _.flatten(Array.prototype.map.call(str,x => x.charCodeAt(0)).map(decompose)).join('')
-}
-function compose(memo,x) {
-    let y = String.fromCharCode(x)
-    if (x < 0x3130 || x > 0x318E) {
-        if (memo.buf) {
-            memo.$ += memo.buf.$
-            memo.buf = null    
-        }
-        memo.$ += y
-        return memo
-    } else {
-        let $$
-        if (memo.buf && memo.buf.length == 3) {
-            if (jong.indexOf(cho[memo.lastCho] + y) < 0) {
-                memo.$ += memo.buf.$
-                $$ = memo.buf.$$
-                memo.buf = null    
-            } else {
-                memo.buf[2] = jong.indexOf(cho[memo.lastCho] + y)
-                memo.lastCho = cho.indexOf(y)
+///////////////////////////////////////
+const CONV_FUNC_TABLE = {
+    conv_i_a2b: (c) => parseInt(((c.charCodeAt(0) - KOREAN_UNICODE_DEFAULT_INDEX) / 28) / 21),
+    conv_m_a2b: (c) => parseInt(((c.charCodeAt(0) - KOREAN_UNICODE_DEFAULT_INDEX) / 28) % 21),
+    conv_t_a2b: (c) => parseInt((c.charCodeAt(0) - KOREAN_UNICODE_DEFAULT_INDEX) % 28) - 1,
+
+    conv_i_adv: (c) => c + parseInt('0x1100', 16),
+    conv_m_adv: (c) => c + parseInt('0x1161', 16),
+    conv_t_adv: (c) => c + parseInt('0x11A8', 16),
+
+    conv_i_b2a: (i) => (i - parseInt('0x1100', 16)) * 588,
+    conv_m_b2a: (i) => (i - parseInt('0x1161', 16)) * 28,
+    conv_t_b2a: (i) => i - parseInt('0x11A8', 16) + 1,
+};
+
+function split_korean_char(char) {
+    return _([
+        CONV_FUNC_TABLE.conv_i_a2b(char),
+        CONV_FUNC_TABLE.conv_m_a2b(char),
+        CONV_FUNC_TABLE.conv_t_a2b(char)
+    ]).filter(c => c > -1) // @INFO : valid check
+        .map((c, idx) => [
+            CONV_FUNC_TABLE.conv_i_adv,
+            CONV_FUNC_TABLE.conv_m_adv,
+            CONV_FUNC_TABLE.conv_t_adv
+        ][idx](c))
+        .map(c => h2Ascii[c])
+        .map(c => String.fromCharCode(c))
+        .value();
+};
+
+function merge_korean_char(chars) {
+    let code_korean_char = KOREAN_UNICODE_DEFAULT_INDEX;
+    _.map([
+        CONV_FUNC_TABLE.conv_i_b2a,
+        CONV_FUNC_TABLE.conv_m_b2a,
+        CONV_FUNC_TABLE.conv_t_b2a,
+    ], (f, idx) => {
+        if (chars[idx]) code_korean_char += f.call(undefined, ascii2H[chars[idx].charCodeAt(0)]);
+    })
+    return String.fromCharCode(code_korean_char);
+};
+///////////////////////////////////////
+// @WARN : can't support all of ascii code set('{', '|', '}')
+function encode(lines) {
+    let korean_flag = false;
+    return _.flatten(_.map(lines, char => {
+        // @INFO : we need to convert only korean chracter!
+        // @WARN : It doesn't vouch not perfact korean character (ex. 'ㄱ', 'ㅓ')
+        if (LAST_KOREAN_CHAR >= char && char >= FIRST_KORAEN_CHAR) {
+            let list = split_korean_char(char);
+            list.unshift('|');
+            if (!korean_flag) {
+                list.unshift('{');
+                korean_flag = true;
             }
+            return list;
         }
-        if (memo.buf) {
-            if (memo.buf.length == 1) {
-                let yy = jung.indexOf(y)
-                if (yy < 0) {
-                       
-                } else {
-                    memo.buf.push(yy)    
-                }
-            } else if (memo.buf.length == 2) {
-                let yy = jong.indexOf(y)
-                if (yy < 0) {
-                    
-                } else {
-                    memo.lastCho = cho.indexOf(y)
-                    memo.buf.push(yy)   
-                }
+        else {
+            let list = [char];
+            if ((char > ' ') && korean_flag) {
+                korean_flag = false;
+                list.unshift('}');
             }
-        } else {
-            if (jung.indexOf(y) < 0) {
-                let yy = cho.indexOf(y)
-                if (yy < 0) {
-                    
-                } else {
-                    memo.buf = [ yy ]    
-                }
-            } else if (memo.lastCho && $$) {
-                memo.$ = memo.$.substr(0,memo.$.length-1) + $$
-                memo.buf = [ memo.lastCho, jung.indexOf(y) ]
-            } else {
-                return memo
-            }
+            return list;
         }
-        memo.buf.$$ = memo.buf.$
-        memo.buf.$ = String.fromCharCode(0xac00 + ((memo.buf[0] * 21 ) + memo.buf[1] ) * 28 + ((memo.buf[2]+1) || 0))
-        
-        return memo
-    }
-}
-function detest(str) {
-    let o = _.reduce(Array.prototype.map.call(str,x => x.charCodeAt(0)),compose,{$:''})
-    if (o.buf) {
-        return o.$ + o.buf.$
-    } else {
-        return o.$
-    }
-}
+    })).join('');
+};
 
-let H = _.uniq(_.flatten([cho,jung,_.flatten(jong.map(x => x.split('')))]))
-let A = _.range(0,H.length).map(x => String.fromCharCode((x < 26 ? 65 : 97) +x%26))
-let HtoA = {}
-let AtoH = {}
-_.each(H,(h,i) => HtoA[h] = A[i])
-_.each(A,(h,i) => AtoH[h] = H[i])
-
-function detest2(str) {
-    str = Array.prototype.map.call(str,x => desafe(x)).join('')
-    return detest(str)
-}
-
-function desafe(x) {
-    return AtoH[x] || x
-}
-
-function safe(x) {
-    return HtoA[x] || x
-}
-
-function test2(str) {
-    str = test(str)
-    return Array.prototype.map.call(str,x => safe(x)).join('')
-}
-
-module.exports = {
-    decode: detest2,
-    encode: line => test2(line.replace(/[^\uac00-\ud7af \.\?!\n]+/g,'#'))
-}
-
+function decode(lines) {
+    return lines.replace(/\{([^}]+)\}/g, (str, p1) => {
+        p1 = p1.substr(1); // @INFO : remove garbage first character '|'
+        return _.map(p1.split(/\|| /), t => (t == '') ? ' ' : merge_korean_char(t)).join('');
+    });
+};
---
+++ test/index.js
@@ -0,0 +1,21 @@
+const lib = require('../index');
+
+let txt = `이번 강추위는 북극 주변의 찬 공기를 가둬놓던 제트기류가 지구 온난화로 인해 약화하면서 '북극 한기'가 남쪽으로 이동한 것이 근본 원인이다.
+북극 상공의 찬 기류를 '폴라 보텍스'(polar vortex)라고 부른다. 강한 바람대인 제트기류는 평소 북극 주변을 빠르게 돌면서 이 찬 기류를 막아두는 역할을 한다. 
+그런데 최근 온난화로 인해 북극 해빙이 녹아 북극 상층의 온도가 올라가고 제트기류가 약해지자 북극 한기가 남하해 중위도 지역인 한반도까지 영향을 준 것이다.
+<최강한파> 제주 '섬 속의 섬' 우도도 '눈세상' (제주=연합뉴스) 전지혜 기자 = 1월 24일 제주 전역에 기록적인 폭설이 내린 가운데 관광객들이 많이 찾는 제주 '섬 속의 섬' 우도에도 눈이 하얗게 쌓여 있다. << 우도면사무소 제공 >> atoz@yna.co.kr
+온난화로 인해 '한파 울타리'가 느슨해져 오히려 혹독한 추위가 엄습하는 '온난화의 역설'인 셈이다. 
+대설의 경우 중국 북부지방에서 찬 대륙고기압이 확장하면서 상대적으로 따뜻한 서해상을 중심으로 눈구름이 만들어져 바람을 타고 육상으로 유입돼 발생했다.`
+
+console.log('source text\n', txt)
+let enc = lib.encode(txt)
+console.log('encode text\n', enc)
+let dec = lib.decode(enc)
+console.log('decode text\n', dec)
+
+console.log(`result
+    source len: ${txt.length}
+    encode len: ${enc.length}
+    decode len: ${dec.length}
+    equality: ${txt == dec}
+    loss: ${enc.length / txt.length * 100}`);
